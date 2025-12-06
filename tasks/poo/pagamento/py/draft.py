ec72d90a9dff33be08757eecbed08704d841687e
will-almeidaof <willyamabr@gmail.com>
2025-12-05 21:15:10 -0300

    o preço foi pago

--- tasks/poo/pagamento/py/draft.py
+++ tasks/poo/pagamento/py/draft.py
@@ -26,9 +26,9 @@ class Pix(Pagamento):
 
     def Processar(self):
         if self.validarValor():
-            print(f"Pagamento de R$ {self.valor}: {self.descricao} \nPIX enviado via {self.banco} usando chave {self.chave}")
+            print(f"PIX enviado via {self.banco} usando chave {self.chave}")
         else:
-            print(f"Pagamento de R$ {self.valor}: {self.descricao} \nPIX não enviado via {self.banco} usando chave {self.chave}")
+            print(f"Erro: PIX não enviado para a chave {self.chave}")
 
 
 
@@ -41,26 +41,50 @@ class Credito(Pagamento):
         self.limite = limite
 
     def Processar(self):
+        if not self.validarValor():
+            print(f"Erro: Valor inválido")
+            return
         if self.validarValor():
             if self.valor > self.limite:
-                print(f"Pagamento de R$ {self.valor}: {self.descricao} \nPagamento reprovado no cartão Cliente {self.titular}. Limite restante: {self.limite}")
+                print(f"Erro: Limite insuficiente no cartão {self.numero}")
             else:
-                restante = self.valor - self.limite
+                restante = self.limite - self.valor
                 self.limite = restante
-                print(f"Pagamento de R$ {self.valor}: {self.descricao} \nPagamento aprovado no cartão Cliente {self.titular}. Limite restante: {self.limite}")
-
-
-
-
-
-
-
-
-
-
-pix = Credito(100,"Blusa",66000,"Jose",200)
-pix.Processar()
-
-    
-
+                print(f"Pagamento aprovado no cartão Cliente {self.titular}. Limite restante: {self.limite}")
             
+
+
+
+
+class Boleto(Pagamento):
+    def __init__(self, valor, descricao, codigo: str, vencimento: str):
+        super().__init__(valor, descricao)
+        self.codigo = codigo
+        self.vencimento = vencimento
+
+    def Processar(self):
+        if self.validarValor():
+            print(f"Boleto gerado. Aguardando pagamento...")
+        else:
+            print(f"Boleto não foi gerado. Valor inválido!")
+
+
+def processar_pagamentos(pagamento: Pagamento):
+        pagamento.validarValor()
+        pagamento.resumo()
+        pagamento.Processar()
+
+
+
+
+
+
+pagamentos = [
+    Pix(150, "Camisa esportiva", "email@ex.com", "Banco XPTO"),
+    Credito(400, "Tênis esportivo", "1234 5678 9123 4567", "Cliente X", 500),
+    Boleto(89.90, "Livro de Python", "123456789000", "2025-01-10"),
+    Credito(800, "Notebook", "9999 8888 7777 6666", "Cliente Y", 700),  # deve falhar
+]
+    
+for pagamento in pagamentos:
+    processar_pagamentos(pagamento)
